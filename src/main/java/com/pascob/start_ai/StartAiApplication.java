4645ec835e357130564f5d5f18748ee2d9a1678e
pascob <pascob.pk@gmail.com>
2024-08-19 11:00:02 +0000

    first commit

---
+++ src/main/java/com/pascob/start_ai/StartAiApplication.java
@@ -0,0 +1,13 @@
+package com.pascob.start_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pascob/start_ai/StartAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascob.start_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
